888914ad71a39bf2edb91a968c44704a742f54cf
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2024-12-05 16:46:09 +0100

    Day 5 solved

--- day_5/main.ml
+++ day_5/main.ml
@@ -1,39 +1,70 @@
 open Core
 
-let is_correct rules ordering_arr =
+let is_correct rules ordering =
+  let rules_arr = Array.init 100 ~f:(fun _ -> Set.empty (module Int)) in
+  List.iter rules ~f:(fun (a, b) -> rules_arr.(a) <- Set.add rules_arr.(a) b);
+  let ordering_arr = Array.of_list ordering in
   let cur_set = ref @@ Set.empty (module Int) in
   let is_correct = ref true in
   for i = 0 to Array.length ordering_arr - 1 do
-    let inter = Set.inter !cur_set rules.(ordering_arr.(i)) in
-    if (Set.length inter) > 0 then (
-      is_correct := false)
-    else ();
+    let inter = Set.inter !cur_set rules_arr.(ordering_arr.(i)) in
+    if Set.length inter > 0 then is_correct := false else ();
     cur_set := Set.add !cur_set ordering_arr.(i);
     ()
   done;
   !is_correct
 
-
 let get_middle_if_correct rules ordering =
   let ordering_arr = Array.of_list ordering in
-  let is_correct = is_correct rules ordering_arr in
+  let is_correct = is_correct rules ordering in
   if not is_correct then 0 else ordering_arr.(Array.length ordering_arr / 2)
 
 let solve_1 (rules, orderings) =
-  let rules_arr = Array.init 100 ~f:(fun _ -> Set.empty (module Int)) in
-  List.iter rules ~f:(fun (a, b) -> rules_arr.(a) <- Set.add rules_arr.(a) b);
   let result =
-    List.map orderings ~f:(fun o -> get_middle_if_correct rules_arr o)
+    List.map orderings ~f:(fun o -> get_middle_if_correct rules o)
     |> List.fold ~init:0 ~f:( + )
   in
   printf "%d\n" result
 
-let solve_2 (rules, orderings) = 
-  let rules_arr = Array.init 100 ~f:(fun _ -> Set.empty (module Int)) in
-  List.iter rules ~f:(fun (a, b) -> rules_arr.(a) <- Set.add rules_arr.(a) b);
+let topo_sort rules ordering =
+  let vertices =
+    List.fold ordering
+      ~init:(Set.empty (module Int))
+      ~f:(fun acc x -> Set.add acc x)
+  in
+  let incoming_edges = Array.init 100 ~f:(const 0) in
+  List.iter rules ~f:(fun (a, b) ->
+      if Set.mem vertices a && Set.mem vertices b then
+      incoming_edges.(b) <- incoming_edges.(b) + 1);
+  let edges = Array.init 100 ~f:(fun _ -> Set.empty (module Int)) in
+  List.iter rules ~f:(fun (a, b) ->
+      if Set.mem vertices a && Set.mem vertices b then
+        edges.(a) <- Set.add edges.(a) b);
+  let to_consider = Queue.create () in
+  Set.iter vertices ~f:(fun v ->
+      if equal incoming_edges.(v) 0 then
+        Queue.enqueue to_consider v);
+  let result = Dynarray.create () in
+  while not @@ Queue.is_empty to_consider do
+    let front = Queue.dequeue_exn to_consider in
+    Dynarray.add_last result front;
+    Set.iter edges.(front) ~f:(fun v -> 
+      incoming_edges.(v) <- incoming_edges.(v) - 1;
+      if equal incoming_edges.(v) 0 then
+        Queue.enqueue to_consider v
+      );
+  done;
+  Dynarray.to_list result
+
+let solve_2 (rules, orderings) =
+  let incorrect =
+    List.filter orderings ~f:(fun o -> not @@ is_correct rules o)
+  in
+  let toposorted = List.map incorrect ~f:(fun o -> topo_sort rules o) in
   let result =
-    List.map orderings ~f:(fun o -> get_middle_if_correct rules_arr o)
-    |> List.fold ~init:0 ~f:( + )
+    List.fold toposorted ~init:0 ~f:(fun acc l ->
+        let arr = List.to_array l in
+        acc + arr.(Array.length arr / 2))
   in
   printf "%d\n" result
 
